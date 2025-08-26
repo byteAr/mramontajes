029977b9fa8fcd21861782bf9008bcba341b0beb
Marcos lopez <marcosmatiaslopez.2018@gmail.com>
2025-08-26 18:34:20 -0300

    add neurobyte and closing project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { GaleryComponent } from './pages/galery/galery.component';
 import { MainComponent } from './shared/main/main.component';
 
 export const routes: Routes = [
-  {path: '', component: MainComponent, title: 'Home'},
-  {path: 'about', component: AboutComponent, title: 'Home'},
-  {path: 'gallery', component: GaleryComponent, title: 'Home'},
+  {path: '', component: MainComponent, title: 'MRA MONTAJES'},
+  {path: 'about', component: AboutComponent, title: 'SOBRE NOSOTROS'},
+  {path: 'gallery', component: GaleryComponent, title: 'GALERIA'},
   {path: '**', redirectTo: ''},
 ];
